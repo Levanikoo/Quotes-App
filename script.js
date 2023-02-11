c6eeaacef4d5d1efb3fee7695042b933a81a2aa4
Levanikoo <124919799+Levanikoo@users.noreply.github.com>
2023-02-11 07:54:03 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,83 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
+
+
+let apiQuotes = []; 
+//show loading
+function loading() {
+    loader.hidden = false;
+    quoteContainer.hidden = true
+}
+// hide loading
+function complete() {
+    quoteContainer.hidden = false
+    loader.hidden = true
+}
+
+
+
+
+//Show New Quote
+function newQuote() {
+    loading();
+    //Pick random quote from apiQuotes array
+  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)]; 
+ //check if author field is blank and replace it with 'Unknown'
+
+  if (!quote.author) {
+    authorText.textContent = 'Unknown'
+  } else {
+    authorText.textContent = quote.author;
+  }
+//check Quote length determine styling
+if (quote.text.length > 120){
+   quoteText.classList.add('long-quote');
+} else{
+    quoteText.classList.remove('long-quote');
+}
+// set quote, hide loader
+  quoteText.textContent = quote.text;
+  complete();
+}
+
+
+
+
+
+//get Quoutes for api//
+async function getQuotes(){
+
+    loading();
+   const apiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
+    
+   try {
+    const response = await fetch(apiUrl);
+   apiQuotes =await response.json();
+    newQuote();
+    } catch (error) {
+    console.log('error')
+    }
+}
+//tweeT Quote
+function tweetQuote() {
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.
+    textContent} - ${authorText.textContent}`;
+    window.open(twitterUrl, '_blank');
+}
+//event listeners
+newQuoteBtn.addEventListener('click', newQuote);
+twitterBtn.addEventListener('click', tweetQuote)
+
+
+
+
+
+//on Load
+
+getQuotes();
+
+
